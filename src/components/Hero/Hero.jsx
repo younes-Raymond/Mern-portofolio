96151294687181a320441525aff5f3be68483f50
younes1337 <raymondyounes2@gmail.com>
2024-01-12 01:00:16 +0100

    Hey There,

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -15,63 +15,69 @@ const Hero = () => {
   
   return (
     <section className={`paddings ${css.wrapper}`}>
-      <motion.div
-        variants={staggerContainer}
-        initial="hidden"
-        whileInView="show"
-        viewport={{ once: false, amount: 0.25 }}
-        className={`innerWidth ${css.container}`}
-      >
-        <div className={css.upperElements}>
-          <motion.span className="primaryText" variants={fadeIn("right", "tween", 0.2, 1)}>
-            Hey There,
-            <br />
-            I'm Younes Raymond.
-          </motion.span>
-          <motion.span className="secondaryText"variants={fadeIn("left", "tween", 0.4, 1)}>
-            I'm a MERN stack developer.
-            <br />
-            I love building web applications.
-            <br /> 
-            and solving problems.
-          </motion.span>
-        </div>
+     <motion.div
+  variants={staggerContainer}
+  initial="hidden"
+  whileInView="show"
+  viewport={{ once: false, amount: 0.25 }}
+  className={`innerWidth ${css.container}`}
+>
+  <div className={css.upperElements}>
+    <motion.span className="primaryText" variants={fadeIn("right", "tween", 0.2, 1)}>
+      Hey There,
+      <br />
+      I'm Younes Raymond.
+    </motion.span>
+    <motion.span className="secondaryText" variants={fadeIn("left", "tween", 0.4, 1)}>
+       Full Stack Web developer.
+      <br />
+      I love building web applications.
+      <br />
+      and solving problems.
+    </motion.span>
+  </div>
 
-        <motion.div
-          variants={fadeIn("up", "tween", 0.3, 1)}
-          className={css.person}
-        >
-          <motion.img variants={slideIn("up", "tween", 0.5, 1.3)} src="./person.png" alt="" />
-        </motion.div>
+  <motion.div
+    variants={fadeIn("up", "tween", 0.3, 1)}
+    className={css.person}
+  >
+    <motion.img variants={slideIn("up", "tween", 0.5, 1.3)} />
+  </motion.div>
 
-     <motion.div 
-     
-     >
-      <a href="https://www.linkedin.com/in/younes-raymond-188a40241/" target="_blanck">
-      <motion.button  >
-        Hire Me 
-      </motion.button>
-      </a>
-     </motion.div>
-        <a href="/web developer.pdf" download>
-         <motion.button>Download Me </motion.button>
-         </a>
-        <div className={css.lowerElements}>
-          <motion.div variants={fadeIn("right", "tween", 0.3, 1)} className={css.experience}>
-            <div className="primaryText">3</div>
-            <div className="secondaryText">
-              <div>Years</div>
-              <div>Experience</div>
-            </div>
-          </motion.div>
+  {/* Mention your volunteering background */}
+  <motion.span className="secondaryText" variants={fadeIn("top", "tween", 0.4, 1)}>
 
-          <motion.div variants={fadeIn("left", "tween", 0.5, 1)} className={css.certificate}>
-            <img src="./certificate.png" alt="" />
-            <span>CERTIFIED PROFESSIONAL</span>
-            <span>MERN STACK DEVELOPER</span>
-          </motion.div>
-        </div>
-      </motion.div>
+    <span className="t">Volunteering Background</span>
+   
+  </motion.span>
+
+  <motion.div variants={fadeIn("up", "tween", 0.3, 1)} className={css.volunteering}>
+  <span className="secondaryText">
+      Actively contributed to volunteer projects, including earthquake relief efforts in Morocco.
+      <br />
+      Collaborated with diverse teams and project managers to enhance user experiences and deliver innovative solutions.
+    </span>   
+  </motion.div>
+
+  
+
+  <div className={css.lowerElements}>
+    <motion.div variants={fadeIn("right", "tween", 0.3, 1)} className={css.experience}>
+      <div className="primaryText">3</div>
+      <div className="secondaryText">
+        <div>Years</div>
+        <div>Experience</div>
+      </div>
+    </motion.div>
+
+    <motion.div variants={fadeIn("left", "tween", 0.5, 1)} className={css.certificate}>
+      <img src="./certificate.png" alt="" />
+      <span>CERTIFIED PROFESSIONAL</span>
+      <span>IBM FULL STACK WEB DEVELOPER</span>
+    </motion.div>
+  </div>
+</motion.div>
+
     </section>
   );
 };
